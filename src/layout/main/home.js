e72c68a65be9c0397d8599a9d5816a9df3f82f24
cookk <kwonjae13@gmail.com>
2022-09-06 21:50:18 +0900

    'Deploy'

--- src/layout/main/home.js
+++ src/layout/main/home.js
@@ -16,7 +16,7 @@ function Home() {
               현재 지속적인 개발 중이니 많은 관심 부탁드립니다.<br></br>
               특성/스킬은 기본적인 번역 이후 구글 번역 API를 사용해 우선 번역해두었고,<br></br>
               이후 하나씩 직접 번역한 내용으로 업데이트 할 예정입니다.<br></br>
-              의견 및 도움주실 분들께서는 <a href="https://discord.gg/m8XwaW4waa">개발/번역 디스코드</a>로 참여 부탁드립니다.
+              의견 및 도움주실 분들께서는 <a href="https://discord.gg/m8XwaW4waa" target="_blank">개발/번역 디스코드</a>로 참여 부탁드립니다.
             </Card.Body>
           </Card>
         </Col>
